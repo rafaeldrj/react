c514903ef91a4a65b1f4ab7bc21d6ceea3e0c68f
Rafael Gonçalves <rafael.drj@outlook.com>
2024-08-08 17:08:26 -0300

    criando a sidebar

--- projetos/projetos-01/01-fundamentos-reactjs/src/App.jsx
+++ projetos/projetos-01/01-fundamentos-reactjs/src/App.jsx
@@ -2,18 +2,24 @@ import { Header } from "./components/Header"
 import { Post } from "./Post"
 
 import "./global.css"
+import style from "./App.module.css"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   // Se tiver mais return, precisa colocar em uma div.
   return (
     <div>
       <Header />
-
-      <Post
-        author="Rafael Goncalves"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus inventore eveniet deleniti veniam reprehenderit error officiis necessitatibus sapiente officia, molestias cum doloremque laboriosam porro aliquam earum quod fugiat sed itaque!"
-      />
-      <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+      <div className={style.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Rafael Goncalves"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus inventore eveniet deleniti veniam reprehenderit error officiis necessitatibus sapiente officia, molestias cum doloremque laboriosam porro aliquam earum quod fugiat sed itaque!"
+          />
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ projetos/projetos-01/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from "./Sidebar.module.css"
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1563206767-5b18f218e8de?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <strong>Rafael Gonçalves</strong>
+        <span>Web Developer</span>
+
+        <footer>
+          <a href="#">Editar seu perfil</a>
+        </footer>
+      </div>
+    </aside>
+  )
+}
