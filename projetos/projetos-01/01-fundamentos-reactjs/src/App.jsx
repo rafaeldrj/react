c17d72c686485dea781b5dce6ccf54fd19c5abf7
Rafael Gonçalves <rafael.drj@outlook.com>
2024-08-08 11:18:12 -0300

    aprendendo CSS Modules

--- projetos/projetos-01/01-fundamentos-reactjs/src/App.jsx
+++ projetos/projetos-01/01-fundamentos-reactjs/src/App.jsx
@@ -1,3 +1,4 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
 
 import "./styles.css"
@@ -6,6 +7,8 @@ export function App() {
   // Se tiver mais return, precisa colocar em uma div.
   return (
     <div>
+      <Header />
+
       <Post
         author="Rafael Goncalves"
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus inventore eveniet deleniti veniam reprehenderit error officiis necessitatibus sapiente officia, molestias cum doloremque laboriosam porro aliquam earum quod fugiat sed itaque!"
